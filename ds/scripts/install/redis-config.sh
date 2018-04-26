fbda59b7f73cbaa4b28ddd07f02c39717cd58a2c
RadZaeem <you@example.com>
2018-04-26 23:28:58 +0900

    fix

--- ds/scripts/install/redis-config.sh
+++ ds/scripts/install/redis-config.sh
@@ -7,12 +7,12 @@ then
 	$drush @local_proj dl redis
 	echo "
 // Redis settings
-$conf['redis_client_interface'] = 'PhpRedis';
-$conf['redis_client_host'] = '$REDIS_HOST';
-$conf['lock_inc'] = 'sites/all/modules/redis/redis.lock.inc';
-$conf['path_inc'] = 'sites/all/modules/redis/redis.path.inc';
-$conf['cache_backends'][] = 'sites/all/modules/redis/redis.autoload.inc';
-$conf['cache_default_class'] = 'Redis_Cache';
+\$conf['redis_client_interface'] = 'PhpRedis';
+\$conf['redis_client_host'] = '$REDIS_HOST';
+\$conf['lock_inc'] = 'sites/all/modules/redis/redis.lock.inc';
+\$conf['path_inc'] = 'sites/all/modules/redis/redis.path.inc';
+\$conf['cache_backends'][] = 'sites/all/modules/redis/redis.autoload.inc';
+\$conf['cache_default_class'] = 'Redis_Cache';
 " >> /host/var-www/proj/sites/default/settings.php 
 
 	service apache2 restart
